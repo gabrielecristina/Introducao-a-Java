ee9bca02624567346031020b20f1fff719ceeb6b
Gabriele Cristina <54123623+gabrielecristina@users.noreply.github.com>
2022-08-05 19:17:29 -0300

    Add files via upload

---
+++ TestaCondicional2.java
@@ -0,0 +1,14 @@
+
+public class TestaCondicional2 {
+
+	public static void main(String[] args) {
+		int idade = 16;
+		boolean acompanhado = true;
+		if (idade >= 18 || acompanhado) {
+			System.out.println("Seja bem vindo! ");
+
+		} else {
+			System.out.println("Menor de idade");
+		}
+	} 
+}
