e2094b973efd96c4d8e2fda3273354b8e7f83317
Gabriele Cristina <54123623+gabrielecristina@users.noreply.github.com>
2022-08-09 12:17:44 -0300

    Add files via upload

---
+++ Estruturas Condicionais/TesteFor.java
@@ -0,0 +1,12 @@
+
+public class TesteFor {
+
+	public static void main(String[] args) {
+		for (int contador = 0; contador <=10; contador++) {
+			System.out.println(contador);
+			
+		}
+		System.out.println(contador); // o For nao permite execução da variavel fora do seu escopo
+	}
+
+}
--- Estruturas Condicionais/TesteIR.java
+++ Estruturas Condicionais/TesteIR.java
@@ -2,17 +2,48 @@
 public class TesteIR {
 
 	public static void main(String[] args) {
-		double salario = 3300.0;
 
-		if (salario >= 2800.0) {
-			System.out.println("o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142");
-
-		} else if (salario >= 3751.0) {
-			System.out.println("o IR é de 15% e pode deduzir R$ 350");
-		} else {
-			System.out.println("De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636");
+		int mes = 10;
+		switch (mes) {
+		case 1:
+			System.out.println("O mês é Janeiro");
+			break;
+		case 2:
+			System.out.println("O mês é Fevereiro");
+			break;
+		case 3:
+			System.out.println("O mês é Março");
+			break;
+		case 4:
+			System.out.println("O mês é Abril");
+			break;
+		case 5:
+			System.out.println("O mês é Maio");
+			break;
+		case 6:
+			System.out.println("O mês é Junho");
+			break;
+		case 7:
+			System.out.println("O mês é Julho");
+			break;
+		case 8:
+			System.out.println("O mês é Agosto");
+			break;
+		case 9:
+			System.out.println("O mês é Setembro");
+			break;
+		case 10:
+			System.out.println("O mês é Outubro");
+			break;
+		case 11:
+			System.out.println("O mês é Novembro");
+			break;
+		case 12:
+			System.out.println("O mês é Dezembro");
+			break;
+		default:
+			System.out.println("Mês inválido");
+			break;
 		}
-
 	}
-
 }
