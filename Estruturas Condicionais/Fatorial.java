22e9ef93f8bf648aa8b4db24f8b30594d203d51b
Gabriele Cristina <54123623+gabrielecristina@users.noreply.github.com>
2022-08-09 13:29:49 -0300

    Add files via upload

---
+++ Estruturas Condicionais/Fatorial.java
@@ -0,0 +1,14 @@
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		int fatorial = 1;
+		for (int n = 1; n <= 11; n++) {
+			fatorial *= n;
+
+			System.out.println("O Fatorial é " + n + " = " + fatorial);
+
+		}
+	}
+
+}
---
+++ Estruturas Condicionais/MultiplodeTres.java
@@ -0,0 +1,15 @@
+
+public class MultiplodeTres {
+
+	public static void main(String[] args) {
+
+		for (int numero = 0; numero <= 100; numero++) {
+			if (numero % 3 == 0) {
+				System.out.println(numero);
+
+			}
+
+		}
+
+	}
+}
